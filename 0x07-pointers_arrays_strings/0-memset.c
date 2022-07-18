ffa87c38aed30940b64c192cc4aaeb8303fc39cc
rogenaantony <rogenaantonyowande@gmail.com>
2022-07-18 03:48:20 -0700

    n

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -7,10 +7,12 @@
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i;
+	char *start = s;
 
-	for (i = 0; i < n; i++)
-		*(s + i) =  b;
-
-	return (s);
+	while (n--)
+	{
+		*s = b;
+		s++;
+	}
+	return (start);
 }
