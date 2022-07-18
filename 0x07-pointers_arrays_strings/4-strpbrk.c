0a5d4c0347213f19a60fa16bd186294dca66a31e
rogenaantony <rogenaantonyowande@gmail.com>
2022-07-18 05:45:23 -0700

    lpo

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -2,9 +2,10 @@
 
 /**
  * _strpbrk - searches a string for any of a set of bytes.
- * @s == first string
- * @accept == second string
- * return == a pointer to the in s that matches one of the byte in accept or null if such byte is found
+ * @s: first string.
+ * @accept: second string.
+ * Return: a pointer to the byte in s that matches one of the
+ * bytes in accept, or NULL if no such byte is found.
  */
 char *_strpbrk(char *s, char *accept)
 {
